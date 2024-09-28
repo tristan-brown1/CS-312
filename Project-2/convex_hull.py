e68d53ed8273fa62ff67fb11f6e26cb787064a8b
Tristan Brown <2tristanbrown@gmail.com>
2024-09-27 21:34:02 -0600

    Update convex_hull.py

--- Project-2/convex_hull.py
+++ Project-2/convex_hull.py
@@ -3,9 +3,16 @@
 from plotting import draw_line, draw_hull, circle_point
 
 
+def node():
+    def __init__(self,x, y, right, left):
+        self.x = x
+        self.y = y
+        self.right = right
+        self.left = left
+
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the subset of provided points that define the convex hull"""
 
 
-    
+
     return []
