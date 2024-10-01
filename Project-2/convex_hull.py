73f5fdb50b6623051f2812c18d6854da7f1e1887
Tristan Brown <2tristanbrown@gmail.com>
2024-10-01 17:23:43 -0600

    Update convex_hull.py

--- Project-2/convex_hull.py
+++ Project-2/convex_hull.py
@@ -4,17 +4,17 @@ from plotting import draw_line, draw_hull, circle_point
 
 
 class Node:
-    def __init__(self,x, y, right=0, left=0):
+    def __init__(self,x, y, clockwise=0, counter_clockwise=0):
         self.x = x
         self.y = y
-        self.right = right
-        self.left = left
+        self.clockwise = clockwise
+        self.counter_clockwise = counter_clockwise
 
-    def set_right(self, new_node):
-        self.right = new_node
+    def set_clockwise(self, new_node):
+        self.clockwise = new_node
 
-    def set_left(self, new_node):
-        self.left = new_node
+    def set_counter_clockwise(self, new_node):
+        self.counter_clockwise = new_node
 
     def getX(self):
         return self.x
@@ -22,42 +22,120 @@ class Node:
     def getY(self):
         return self.y
     
-    def get_right(self):
-        return self.right
+    def get_clockwise(self):
+        return self.clockwise
     
-    def get_left(self):
-        return self.left
+    def get_counter_clockwise(self):
+        return self.counter_clockwise
 
 
 def calculate_slope(c: Node,d: Node):
-    return (c.getX - d.getX)/(c.getY - d.getY)
+    return (d.getY - c.getY)/(d.getX - c.getX)
 
-def calculate_highest_slope(a: Node, b: Node):
-    a_mod = a
-    a_mod2 = a
+# def calculate_highest_slope(a: Node, b: Node):
+#     a_mod = a
+#     a_mod2 = a
     
-    a_mod.set_right(b)
-    a_mod2.set_right(b.get_right)
+#     a_mod.set_clockwise(b)
+#     a_mod2.set_clockwise(b.get_clockwise)
 
-    if (calculate_slope(a_mod,a_mod.get_right) >= calculate_slope(a_mod2,a_mod2.get_right)):
-        a.set_right(a_mod.get_right)
-    else:
-        a.set_right(a_mod2.get_right)
+#     if (calculate_slope(a_mod,a_mod.get_clockwise) >= calculate_slope(a_mod2,a_mod2.get_clockwise)):
+#         a.set_clockwise(a_mod.get_clockwise)
+#     else:
+#         a.set_clockwise(a_mod2.get_clockwise)
 
-    return a 
+#     return a 
 
 def calculate_upper(a: Node, b: Node):
 # this part will handle the lower bound calculations
-    a = calculate_highest_slope(a,b)
-    b = calculate_highest_slope(b,a)
+
+    # a = calculate_highest_slope(a,b)
+    # b = calculate_highest_slope(b,a)
+    a_mod = a
+    a_mod2 = a
+    
+    a_changed = True
+    b_changed = True
+    while  a_changed == True or b_changed == True:
+        
+        while a_changed == True :
+            a_changed = False
+            a_mod.set_clockwise(b)
+            a_mod2.set_clockwise(b.get_clockwise)
+
+            if (calculate_slope(a_mod,a_mod.get_clockwise) >= calculate_slope(a_mod2,a_mod2.get_clockwise)):
+                a.set_clockwise(a_mod.get_clockwise)
+                
+            else:
+                a.set_clockwise(a_mod2.get_clockwise)
+                a_changed = True
+
+        b_mod = a.get_clockwise
+        b_mod2 = a.get_clockwise
+
+        while b_changed == True:
+            b_changed = False
+            b_mod.set_counter_clockwise(a)
+            b_mod2.set_counter_clockwise(a.get_counter_clockwise)
+
+            if (calculate_slope(b_mod,b_mod.get_counter_clockwise) >= calculate_slope(b_mod2,b_mod2.get_counter_clockwise)):
+                b.set_counter_clockwise(b_mod.get_counter_clockwise)
+                
+            else:
+                b.set_counter_clockwise(b_mod2.get_counter_clockwise)
+                b_changed == True
+
+        a_mod = b.get_counter_clockwise
+        a_mod2 = b.get_counter_clockwise
 
     return a,b
 
-def calculate_lower():
-# this part will handle the upper bound calculations
+def calculate_lower(a: Node, b: Node):
+# this part will handle the lower bound calculations
+
+    a_mod = a
+    a_mod2 = a
+    
+    a_changed = True
+    b_changed = True
+    while  a_changed == True or b_changed == True:
+        
+
+            ###KEEP WORKING FROM HERE ON CLOCKWISE V COUNTER CLOCKWISE
 
 
-    pass
+
+        while a_changed == True :
+            a_changed = False
+            a_mod.set_counter_clockwise(b)
+            a_mod2.set_counter_clockwise(b.get_counter_clockwise)
+
+            if (calculate_slope(a_mod,a_mod.get_counter_clockwise) <= calculate_slope(a_mod2,a_mod2.get_counter_clockwise)):
+                a.set_counter_clockwise(a_mod.get_counter_clockwise)
+                
+            else:
+                a.set_clockwise(a_mod2.get_clockwise)
+                a_changed = True
+
+        b_mod = a.get_clockwise
+        b_mod2 = a.get_clockwise
+
+        while b_changed == True:
+            b_changed = False
+            b_mod.set_counter_clockwise(a)
+            b_mod2.set_counter_clockwise(a.get_counter_clockwise)
+
+            if (calculate_slope(b_mod,b_mod.get_counter_clockwise) >= calculate_slope(b_mod2,b_mod2.get_counter_clockwise)):
+                b.set_counter_clockwise(b_mod.get_counter_clockwise)
+                
+            else:
+                b.set_counter_clockwise(b_mod2.get_counter_clockwise)
+                b_changed == True
+
+        a_mod = b.get_counter_clockwise
+        a_mod2 = b.get_counter_clockwise
+
+    return a,b
 
 def calculate_rightmost(node_list: list[Node]):
     rightmost_node = None
@@ -97,9 +175,11 @@ def hull_algorithm(node_list: list[Node]):
     leftmost_node = calculate_leftmost(right_hull)
     rightmost_node = calculate_rightmost(left_hull)
 
-    calculate_upper
     
-    calculate_lower
+    corrected_top_hull = calculate_upper(leftmost_node,rightmost_node)
+    
+    corrected_bot_hull = calculate_lower(leftmost_node,rightmost_node)
+
 
 
     return node_list
@@ -110,8 +190,8 @@ def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]
     result_list = []
     for element in sorted(points):
         new_node = Node(element[0], element[1])
-        new_node.set_right(new_node)
-        new_node.set_left(new_node)
+        new_node.set_clockwise(new_node)
+        new_node.set_counter_clockwise(new_node)
         linked_list.append(new_node)
 
 
