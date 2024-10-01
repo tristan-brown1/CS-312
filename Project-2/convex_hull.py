f3634d8c0c2cd337963aac5bd42e88eb081b67e8
Tristan Brown <2tristanbrown@gmail.com>
2024-09-30 21:17:23 -0600

    Update convex_hull.py

--- Project-2/convex_hull.py
+++ Project-2/convex_hull.py
@@ -3,8 +3,8 @@
 from plotting import draw_line, draw_hull, circle_point
 
 
-def node():
-    def __init__(self,x, y, right, left):
+class Node:
+    def __init__(self,x, y, right=0, left=0):
         self.x = x
         self.y = y
         self.right = right
@@ -17,26 +17,43 @@ def node():
 
 def calculate_upper():
 # this part will handle the lower bound calculations
+
+
     pass
 
 def calculate_lower():
 # this part will handle the upper bound calculations
+
+
     pass
 
-def hull_algorithm():
+def hull_algorithm(node_list: list[Node]):
 # this part will handle the linked list and recurse
-    pass
+    n = len(node_list)
+    if n == 1:
+        return node_list
+
+    L = node_list[0:n/2]
+    R = node_list[n/2:]
+
+    left_hull = hull_algorithm(L)
+    right_hull = hull_algorithm(R)
+
+    
+
+
+    return node_list
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the subset of provided points that define the convex hull"""
     linked_list = []
     result_list = []
-    for tuple in list:
-        linked_list.append(node(tuple[0],tuple[1]))
+    for element in sorted(points):
+        linked_list.append(Node(element[0],element[1]))
 
     linked_list = hull_algorithm(linked_list)
 
     for node in linked_list:
-        result_list[node.getX,node.getY]
+        result_list[node.getX(),node.getY()]
 
-    return [result_list]
+    return result_list
