c51f7631d835568cdad4ad5a516119593ee7187f
Tristan Brown <2tristanbrown@gmail.com>
2024-10-14 21:32:39 -0600

    Update network_routing.py

--- Project-3/network_routing.py
+++ Project-3/network_routing.py
@@ -3,18 +3,16 @@ class Array_priority_queue:
 
     def _init_(self, nodes):
         self.prio_queue = dict()
-        i = 0
         for node in nodes:
-            self.prio_queue[i] = node
-            i += 1
+            self.prio_queue[node[0]] = None
 
     def delete_min(self):
         candidate = min(self.prio_queue, key = self.prio_queue.get)
         del self.prio_queue[candidate]
         return candidate
 
-    def decrease_key(self, v):
-        pass
+    def decrease_key(self,node, v):
+        self.prio_queue[node] = v
 
 
 
@@ -23,17 +21,21 @@ def dijkstra(graph,source) -> tuple[list[int], list[int]]:
     dist = dict()
     prev = dict()
     for u in graph:
-        dist[u] = None
-        prev[u] = None
-    dist[source] = 0
+        dist[u[0]] = None
+        prev[u[0]] = None
     H = Array_priority_queue(graph)
+    dist[source] = 0
+    H.decrease_key(source,0)
     while len(H) != 0:
         u = H.delete_min()
-        for u,v in graph:
-            if dist[v] > dist[u] + len(u,v):
-                dist[v] = dist[u] + len(u,v)
-                prev[v] = u 
-                H.decrease_key(v)
+        for connected_node,connection_distance in graph[u]:
+            if dist[connected_node] == None:
+                dist[connected_node] = connection_distance
+                H.decrease_key(connected_node,connection_distance)
+            elif dist[connected_node] > dist[u] + connection_distance:
+                dist[connected_node] = dist[u] + connection_distance
+                prev[connected_node] = u 
+                H.decrease_key(connected_node,connection_distance)
     return dist,prev
 
 
@@ -71,10 +73,11 @@ def find_shortest_path_with_array(
     """
 
     dist, prev = dijkstra(graph, source)
+    
     cost = dist[target]
     path = []
     step = target
-    while step is not None:
+    while step != None:
         path.append(step)
         step = prev[step]
 
