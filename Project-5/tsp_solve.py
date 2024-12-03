9bec1a88a877250bb2e3ae12f1f907bb4b4b217a
Tristan Brown <2tristanbrown@gmail.com>
2024-12-03 00:06:27 -0700

    playing with the idea of shared v not shared

--- Project-5/tsp_solve.py
+++ Project-5/tsp_solve.py
@@ -1,5 +1,8 @@
 import math
 import random
+from email.policy import default
+
+from ply.yacc import token
 
 from tsp_core import Tour, SolutionStats, Timer, score_tour, Solver
 from tsp_cuttree import CutTree
@@ -74,7 +77,6 @@ def greedy_pathfinder(starting_row, edges):
                       index not in visited and weight != float('inf')]
 
         if not candidates:
-            # no valid path remaining
             return None
 
         weight, next_row = min(candidates)
@@ -89,9 +91,11 @@ def greedy_tour(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
     n_nodes_expanded = 0
     n_nodes_pruned = 0
     cut_tree = CutTree(len(edges))
+    cost = float('inf')
+    tour = None
 
     i = 0
-    while True:
+    while i < len(edges):
         if timer.time_out():
             return stats
 
@@ -101,41 +105,56 @@ def greedy_tour(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
             i += 1
             continue
         else:
-            tour = greedy_pathfinder(i, edges)
-            cost = score_tour(tour, edges)
 
-            stats.append(SolutionStats(
-                tour=tour,
-                score=cost,
-                time=timer.time(),
-                max_queue_size=1,
-                n_nodes_expanded=n_nodes_expanded,
-                n_nodes_pruned=n_nodes_pruned,
-                n_leaves_covered=cut_tree.n_leaves_cut(),
-                fraction_leaves_covered=cut_tree.fraction_leaves_covered()
-            ))
+            best_tour = potential_tour
+            best_cost = score_tour(best_tour, edges)
+            if best_cost < cost:
+                tour = best_tour
+                cost = best_cost
+            i += 1
 
-            return stats
+    stats.append(SolutionStats(
+        tour=tour,
+        score=cost,
+        time=timer.time(),
+        max_queue_size=1,
+        n_nodes_expanded=n_nodes_expanded,
+        n_nodes_pruned=n_nodes_pruned,
+        n_leaves_covered=cut_tree.n_leaves_cut(),
+        fraction_leaves_covered=cut_tree.fraction_leaves_covered()
+    ))
+    return stats
 
 
-def dfs_alg(start_node, visited, edges, current_cost, path):
-    current_node = start_node
-    if all(visited):
-        return current_cost + edges[current_node][start_node], path
+def dfs_shared(current_node,current_cost,path,visited,edges,
+               all_paths=None,best_cost=None,best_path=None,prune=False):
 
-    min_cost = float('inf')
-    min_cost_path = []
+    n = len(edges)
 
-    for next_city in range(0, len(edges)):
-        if not visited[next_city]:
-            visited[next_city] = True
-            cost = edges[current_node][next_city]
-            total_cost, sub_path = dfs_alg(next_city, visited, edges, current_cost + cost, path + [next_city])
-            if total_cost < min_cost:
-                min_cost = total_cost
-                min_cost_path = sub_path
-            visited[next_city] = False
-    return min_cost, min_cost_path
+    if len(path) == n:
+        if all_paths is not None:
+            total_cost = current_cost + edges[current_node][path[0]]
+            all_paths.append((total_cost, path[:]))
+        elif current_cost < best_cost[0]:
+            best_cost[0] = current_cost
+            best_path[0] = path[:]
+        return
+
+    for next_node in range(n):
+        if not visited[next_node]:
+            cost_to_next = edges[current_node][next_node]
+
+
+            if prune:
+                estimated_total = current_cost + cost_to_next + calculate_lower_bound(n, visited, edges)
+                if estimated_total >= best_cost[0]:
+                    continue
+
+            visited[next_node] = True
+            dfs_shared(next_node,current_cost + cost_to_next,path + [next_node],visited,edges,
+                all_paths,best_cost,best_path,prune
+            )
+            visited[next_node] = False
 
 def dfs(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
     stats = []
@@ -145,18 +164,16 @@ def dfs(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
 
     start_node = 0
     visited = [False] * len(edges)
-    dfs_answer = dfs_alg(start_node, visited, edges, 0, [])
-    tour = dfs_answer[1]
-    cost = score_tour(tour, edges)
+    visited[start_node] = True
 
+    all_paths = []
+    dfs_shared(start_node, 0, [start_node], visited, edges, all_paths=all_paths)
 
-
-    # take current dfs implementation and move the calculation of the min path to this part of the logic
-
+    min_cost, min_cost_path = min(all_paths, key=lambda x: x[0])
 
     stats.append(SolutionStats(
-        tour=tour,
-        score=cost,
+        tour=min_cost_path,
+        score=min_cost,
         time=timer.time(),
         max_queue_size=1,
         n_nodes_expanded=n_nodes_expanded,
@@ -166,19 +183,6 @@ def dfs(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
     ))
 
     return stats
-
-
-def reduce_matrix(matrix) -> tuple[float, list[SolutionStats]]:
-
-    pass
-
-def expand() -> list[SolutionStats]:
-
-    pass
-
-def bnb_logic(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
-
-    pass
 
 def branch_and_bound(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
     stats = []
@@ -186,26 +190,27 @@ def branch_and_bound(edges: list[list[float]], timer: Timer) -> list[SolutionSta
     n_nodes_pruned = 0
     cut_tree = CutTree(len(edges))
 
-    #clean up dfs logic get it completely fleshed out and clean
-
-    #write out hollow bnb logic using dfs but with updated lower bound in order to prune the queue that feeds into bnb
-
-    #flesh out elements added from bnb
-        #reduce matrix
-        #extracting lower bound
-        #bnb logic
-
-
-
+    n = len(edges)
+    visited = [False] * n
+    best_cost = [float('inf')]
+    best_path = [[]]
 
     start_node = 0
-    visited = [False] * len(edges)
-    cost, tour = dfs_alg(start_node, visited, edges, 0, [])
-    cost = score_tour(tour, edges)
+    visited[start_node] = True
+    dfs_shared(
+        start_node,
+        0,
+        [start_node],
+        visited,
+        edges,
+        best_cost=best_cost,
+        best_path=best_path,
+        prune=True
+    )
 
     stats.append(SolutionStats(
-        tour=tour,
-        score=cost,
+        tour=best_path[0],
+        score=best_cost[0],
         time=timer.time(),
         max_queue_size=1,
         n_nodes_expanded=n_nodes_expanded,
@@ -214,6 +219,205 @@ def branch_and_bound(edges: list[list[float]], timer: Timer) -> list[SolutionSta
         fraction_leaves_covered=cut_tree.fraction_leaves_covered()
     ))
     return stats
+
+
+# def dfs_alg(start_node, visited, edges, current_cost, path, all_paths):
+#     current_node = start_node
+#     path.append(current_node)
+#
+#     if all(visited):
+#         total_cost = current_cost + edges[current_node][path[0]]
+#         all_paths.append((total_cost, path[:]))
+#         path.pop()
+#         return
+#
+#     for next_city in range(len(edges)):
+#         if not visited[next_city]:
+#             visited[next_city] = True
+#             dfs_alg(next_city, visited, edges, current_cost + edges[current_node][next_city], path, all_paths)
+#             visited[next_city] = False
+#
+#     path.pop()
+#
+# def dfs(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
+#     stats = []
+#     n_nodes_expanded = 0
+#     n_nodes_pruned = 0
+#     cut_tree = CutTree(len(edges))
+#
+#     start_node = 0
+#     visited = [False] * len(edges)
+#     visited[start_node] = True
+#
+#     all_paths = []
+#     dfs_alg(start_node, visited, edges, 0, [], all_paths)
+#
+#     min_cost, min_cost_path = min(all_paths, key=lambda x: x[0])
+#
+#     stats.append(SolutionStats(
+#         tour=min_cost_path,
+#         score=min_cost,
+#         time=timer.time(),
+#         max_queue_size=1,
+#         n_nodes_expanded=n_nodes_expanded,
+#         n_nodes_pruned=n_nodes_pruned,
+#         n_leaves_covered=cut_tree.n_leaves_cut(),
+#         fraction_leaves_covered=cut_tree.fraction_leaves_covered()
+#     ))
+#
+#     return stats
+
+
+
+
+def calculate_lower_bound(n, visited, edges):
+    cost = 0
+    for node in range(n):
+        if not visited[node]:
+            min_cost = min(
+                (edges[node][neighbor] for neighbor in range(n) if neighbor != node and not visited[neighbor]),
+                default=0
+            )
+            cost += min_cost
+    return cost
+#
+#
+# def dfs_redone(start_node, current_node, current_cost, path, visited, edges, best_cost, best_path):
+#     n = len(edges)
+#
+#     if len(path) == n:
+#         # A valid Hamiltonian path that touches all nodes
+#         if current_cost < best_cost[0]:
+#             best_cost[0] = current_cost
+#             best_path[0] = path
+#         return
+#
+#     for next_node in range(n):
+#         if not visited[next_node]:
+#             cost_to_next = edges[current_node][next_node]
+#             estimated_total = current_cost + cost_to_next + calculate_lower_bound(n, visited, edges)
+#
+#             # Prune branches with higher cost than current best
+#             if estimated_total < best_cost[0]:
+#                 visited[next_node] = True
+#                 dfs_redone(
+#                     start_node,
+#                     next_node,
+#                     current_cost + cost_to_next,
+#                     path + [next_node],
+#                     visited,
+#                     edges,
+#                     best_cost,
+#                     best_path
+#                 )
+#                 visited[next_node] = False  # Backtrack
+#
+#
+#
+# def branch_and_bound(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
+#     stats = []
+#     n_nodes_expanded = 0
+#     n_nodes_pruned = 0
+#     cut_tree = CutTree(len(edges))
+#
+#     n = len(edges)
+#     visited = [False] * n
+#     best_cost = [float('inf')]  # Use a mutable object to share state across recursive calls
+#     best_path = [[]]  # Use a mutable list to share the best path
+#
+#     start_node = 0
+#     visited[start_node] = True
+#     dfs_redone(start_node, start_node, 0, [start_node], visited, edges, best_cost, best_path)
+#
+#     tour = best_path[0]
+#     cost = best_cost[0]
+#
+#     stats.append(SolutionStats(
+#         tour=tour,
+#         score=cost,
+#         time=timer.time(),
+#         max_queue_size=1,
+#         n_nodes_expanded=n_nodes_expanded,
+#         n_nodes_pruned=n_nodes_pruned,
+#         n_leaves_covered=cut_tree.n_leaves_cut(),
+#         fraction_leaves_covered=cut_tree.fraction_leaves_covered()
+#     ))
+#     return stats
+
+
+
+
+
+
+
+
+# def calculate_lower_bound(n, visited, edges):
+#     cost = 0
+#     for node in range(0, n - 1):
+#         if not visited[node]:
+#             cost += min(edges[node][neighbor] for neighbor in range(0, n - 1) if node != neighbor)
+#
+#     return cost
+#
+# def dfs_redone(start_node, current_node, current_cost, path, visited, edges, best_cost, best_path):
+#     n = len(edges)
+#
+#     if len(path) == n:
+#         total_cost = current_cost + edges[current_node][start_node]
+#
+#         if total_cost < best_cost:
+#             best_cost = total_cost
+#             best_path = path + [start_node]
+#         return best_cost, best_path
+#
+#     for next_node in range(0, n - 1):
+#         if not visited[next_node]:
+#             cost_to_next = edges[current_node][next_node]
+#             estimated_total = current_cost + cost_to_next + calculate_lower_bound(next_node, visited, edges)
+#
+#             if estimated_total < best_cost:
+#                 visited[next_node] = True
+#                 dfs_redone(start_node, next_node, current_cost + cost_to_next, path + [next_node], visited, edges, best_cost,
+#                            best_path)
+#                 visited[next_node] = False
+#
+#     return best_cost, best_path
+#
+#
+#
+# def branch_and_bound(edges: list[list[float]], timer: Timer) -> list[SolutionStats]:
+#     stats = []
+#     n_nodes_expanded = 0
+#     n_nodes_pruned = 0
+#     cut_tree = CutTree(len(edges))
+#     n = len(edges)
+#     visited = [False] * n
+#     best_cost = float('inf')
+#     best_path = []
+#     start_node = 0
+#
+#     visited[start_node] = True
+#     best_cost, best_path = dfs_redone(start_node, start_node, 0, [start_node], visited, edges, best_cost, best_path)
+#
+#
+#
+#     tour = best_path
+#     cost = best_cost
+#
+#     # cost, tour = dfs_alg(start_node, visited, edges, 0, [])
+#     cost = score_tour(tour, edges)
+#
+#     stats.append(SolutionStats(
+#         tour=tour,
+#         score=cost,
+#         time=timer.time(),
+#         max_queue_size=1,
+#         n_nodes_expanded=n_nodes_expanded,
+#         n_nodes_pruned=n_nodes_pruned,
+#         n_leaves_covered=cut_tree.n_leaves_cut(),
+#         fraction_leaves_covered=cut_tree.fraction_leaves_covered()
+#     ))
+#     return stats
 
 
 
